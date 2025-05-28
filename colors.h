1b1baf2991cf78a07a2009004c3962e2c8c6dcfe
s-v-x <Silvia.dfg@gmail.com>
2025-05-28 12:50:34 +0200

    split in two versions

---
+++ colors.h
@@ -0,0 +1,48 @@
+#ifndef COLORS_H
+#define COLORS_H
+
+#include <Arduino.h>
+extern uint16_t light_red[21];
+extern uint16_t red[21];
+extern uint16_t little_red[26];
+
+extern uint16_t green[21];
+extern uint16_t green_2[21];
+extern uint16_t green_3[23];
+extern uint16_t light_green[25];
+extern uint16_t mint[24];
+extern uint16_t light_mint[26];
+extern uint16_t pistacchio[21];
+extern uint16_t little_green[22];
+
+extern uint16_t green_yellow[23];
+extern uint16_t green_yellow_2[23];
+extern uint16_t green_light_yellow[21];
+
+extern uint16_t blue[27];
+extern uint16_t cyan[27];
+extern uint16_t little_blue[27];
+extern uint16_t turqoise[25];
+
+extern uint16_t magenta[37];
+
+extern uint16_t yellow[21];
+extern uint16_t yellow_2[25];
+extern uint16_t warm_yellow[19];
+extern uint16_t light_yellow[25];
+
+extern uint16_t amber[21];
+
+extern uint16_t pink[22];
+extern uint16_t light_pink[23];
+
+extern uint16_t orange[23];
+extern uint16_t orange_2[20];
+extern uint16_t red_orange[22];
+extern uint16_t orange_yellow[24];
+
+extern uint16_t white[23];
+extern uint16_t cream[25];
+extern uint16_t ice[23];
+
+#endif
